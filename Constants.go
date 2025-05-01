faedf1d09f9c04e02124971cba558fcb2740fead
KBNVSATYASAI <2200030313@kluniversity.in>
2025-05-01 18:02:05 +0530

    Go lang basics

---
+++ Constants.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    const Pi = 3.14
+    const Language = "Go"
+
+    fmt.Println("Pi:", Pi)
+    fmt.Println("Language:", Language)
+}
---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "GoLang"
+    var age int = 14
+
+    language := "Go" // shorthand syntax (inside function only)
+
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Language:", language)
+}
